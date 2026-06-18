b60faf96a5f7f8fdb255b264e6fbdb8edb706c7f
Kerolos20E <keroe7500@gmail.com>
2026-06-18 18:31:18 +0300

    add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,27 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [articles, setArticles] = useState([]);
-  async function loadData() {
-    const response =
-      await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${
+  const [query, setQuery] = useState("");
+  async function loadData(inputQuery) {
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?q=${inputQuery}&country=us&apiKey=${
         import.meta.env.VITE_NEWS_FEED_API_KEY
-      }
-    `);
-
+      }`
+    );
     const data = await response.json();
     console.log(data);
-    return data.articles.map((article) => {
+    return (data.articles || []).map((article) => {
       const { title, description, author, publishedAt, urlToImage } = article;
-      return {
-        title,
-        description,
-        author,
-        publishedAt,
-        image: urlToImage,
-      };
+      return { title, description, author, publishedAt, image: urlToImage };
     });
   }
   useEffect(() => {
-    loadData().then(setArticles);
-  }, []);
+    loadData(query).then(setArticles);
+  }, [query]);
+
   return (
     <Container>
-      <NewsHeader />
+      <NewsHeader onSearchChange={setQuery} />
       <NewsFeed articles={articles} />
     </Container>
   );
