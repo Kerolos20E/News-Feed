1f1a16309f236739b14ad081e2bf8114cfc4fb79
Kerolos20E <keroe7500@gmail.com>
2026-06-23 03:24:31 +0300

    Pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import { Container } from "@mui/material";
+import { Container, Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import NewsHeader from "./components/NewsHeader";
 import NewsFeed from "./components/NewsFeed";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import { debounce } from "lodash";
+
+const Footer = styled("div")(({ theme }) => ({
+  margin: theme.spacing(2, 0),
+  display: "flex",
+  justifyContent: "space-between",
+}));
 
 function App() {
   const [articles, setArticles] = useState([]);
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
+  const [pageNumber, setPageNumber] = useState(1);
 
   useEffect(() => {
     const controller = new AbortController();
     setLoading(true);
 
     fetch(
-      `https://newsapi.org/v2/top-headlines?q=${query}&country=us&apiKey=${import.meta.env.VITE_NEWS_FEED_API_KEY}`,
-      { signal: controller.signal },
+      `https://newsapi.org/v2/top-headlines?q=${query}&page=${pageNumber}&pageSize=5&country=us&apiKey=${
+        import.meta.env.VITE_NEWS_FEED_API_KEY
+      }`,
+      { signal: controller.signal }
     )
       .then((res) => res.json())
       .then((data) => {
@@ -26,7 +36,7 @@ function App() {
             author,
             publishedAt,
             image: urlToImage,
-          }),
+          })
         );
         setArticles(newArticles);
         setLoading(false);
@@ -37,19 +47,40 @@ function App() {
       });
 
     return () => controller.abort();
-  }, [query]);
+  }, [query, pageNumber]);
 
-  const handleSearchChange = debounce(
+  const handleSearchChange = useCallback(
     debounce((newQuery) => {
+      setPageNumber(1);
       setQuery(newQuery);
     }, 500),
-    [],
+    []
   );
+
+  const handleNextClick = () => {
+    setPageNumber((prev) => prev + 1);
+  };
+
+  const handlePreviousClick = () => {
+    setPageNumber((prev) => Math.max(1, prev - 1));
+  };
 
   return (
     <Container>
       <NewsHeader onSearchChange={handleSearchChange} />
       <NewsFeed articles={articles} loading={loading} />
+      <Footer>
+        <Button
+          variant="outlined"
+          onClick={handlePreviousClick}
+          disabled={pageNumber === 1}
+        >
+          Previous
+        </Button>
+        <Button variant="outlined" onClick={handleNextClick}>
+          Next
+        </Button>
+      </Footer>
     </Container>
   );
 }
